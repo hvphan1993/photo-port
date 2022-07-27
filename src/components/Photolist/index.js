fe47996f5fd29c9e6e624165cc84134a4fd2b14b
hvphan <hvphan1993@gmail.com>
2022-07-27 10:32:09 -0700

    added modal to enlarge images when clicked

--- src/components/Photolist/index.js
+++ src/components/Photolist/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
+
 
 const PhotoList = ({ category }) => {
-
+  
   const [photos] = useState([
     {
       name: 'Grocery aisle',
@@ -99,17 +101,29 @@ const PhotoList = ({ category }) => {
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     },
   ]);
-
+  
   const currentPhotos = photos.filter((photo) => photo.category === category);
-
+  
+  const [currentPhoto, setCurrentPhoto] = useState();
+  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  const toggleModal = (image, i) => {
+    // current photo
+    setCurrentPhoto({...image, index: i})
+    setIsModalOpen(true);
+  }
+  
   return (
     <div>
+      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
             src={require(`../../assets/small/${category}/${i}.jpg`)}
             alt={image.name}
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
